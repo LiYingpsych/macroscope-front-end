f2298e03818a9d2358e902699afa0ab0626649e9
Jack Crompton <jackcrompton8@gmail.com>
2019-09-24 12:56:58 +0100

    Changed router to browser router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { theme } from './themes';
 import { NotFoundPage } from "./NotFoundPage";
 import { HomePage } from "./HomePage";
@@ -9,12 +9,12 @@ import { HomePage } from "./HomePage";
 const App: React.FC = () => {
     return (
         <MuiThemeProvider theme={theme}>
-            <Router>
+            <BrowserRouter>
                 <Switch>
                     <Route exact={true} path="/" component={HomePage} />
                     <Route component={NotFoundPage} />
                 </Switch>
-            </Router>
+            </BrowserRouter>
         </MuiThemeProvider>
     );
 };
