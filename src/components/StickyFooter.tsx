77b61b16c1b3648a81d7686c108b21608b673d7a
Jack Crompton <jackcrompton8@gmail.com>
2019-09-24 16:03:03 +0100

    Prettier test

--- src/components/StickyFooter.tsx
+++ src/components/StickyFooter.tsx
@@ -3,28 +3,28 @@ import { Theme } from "@material-ui/core/styles";
 import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles((theme: Theme) => ({
-    root: {
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh"
-    },
-    content: {
-        flex: "1",
-    }
+root: {
+display: "flex",
+flexDirection: "column",
+minHeight: "100vh"
+},
+content: {
+flex: "1",
+}
 }));
 
 interface IStickyFooterProps extends React.HTMLAttributes<HTMLDivElement> {
-    content: ReactNode;
-    footer:  ReactNode;
+content: ReactNode;
+footer:  ReactNode;
 }
 
 export const StickyFooter: React.FC<IStickyFooterProps> = (props: IStickyFooterProps) => {
-    const classes = useStyles();
+const classes = useStyles();
 
-    return (
-        <main className={classes.root}>
-            <div className={classes.content}>{props.content}</div>
-            {props.footer}
-        </main>
-    );
+return (
+<main className={classes.root}>
+<div className={classes.content}>{props.content}</div>
+{props.footer}
+</main>
+);
 };
