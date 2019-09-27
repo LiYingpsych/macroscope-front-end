bb6573c311ac9d232f44edd67e31f729aac117e1
Jack Crompton <jackcrompton8@gmail.com>
2019-09-27 22:17:17 +0100

    Added synonyms per target and simalarity threshold

--- src/pages/wordAnalysis/settings/SynonymNetworkSettings.tsx
+++ src/pages/wordAnalysis/settings/SynonymNetworkSettings.tsx
@@ -2,13 +2,24 @@ import React from "react";
 
 import Grid from "@material-ui/core/Grid";
 import YearInput from "../../../components/inputs/YearInput";
+import NumberSelectionInput from "../../../components/inputs/NumberSelectionInput";
 
 import range from "../../../utils/range";
 import { synonymNetworkMinYear, synonymNetworkMaxYear } from "../../../globals";
 
 export default function SynonymNetworkSettings() {
+    // TODO: add additional target words
+
     const years: number[] = range(synonymNetworkMinYear, synonymNetworkMaxYear, 10);
     const defaultYear = years[years.length - 1];
+
+    const synonymsPerTarget: number[] = range(3, 10);
+    const defaultSynonymsPerTarget = 5;
+
+    const simalarityThreshold: number[] = range(50, 100).map((value: number) => {
+        return value / 100;
+    });
+    const defaultSimalarityThreshold = 0.7;
 
     return (
         <form autoComplete="off">
@@ -19,6 +30,22 @@ export default function SynonymNetworkSettings() {
                         defaultYear={defaultYear}
                         onChange={(selectedYear: number) => {
                             console.log(selectedYear);
+                        }}
+                    />
+                    <NumberSelectionInput
+                        label="Synonyms per target"
+                        numbers={synonymsPerTarget}
+                        defaultNumber={defaultSynonymsPerTarget}
+                        onChange={(selectedNUmber: number) => {
+                            console.log(selectedNUmber);
+                        }}
+                    />
+                    <NumberSelectionInput
+                        label="Simalarity threshold"
+                        numbers={simalarityThreshold}
+                        defaultNumber={defaultSimalarityThreshold}
+                        onChange={(selectedNUmber: number) => {
+                            console.log(selectedNUmber);
                         }}
                     />
                 </Grid>
