bd182b6dc4a3958bacff4b9884aed3bd1d6b489e
Jack Crompton <jackcrompton8@gmail.com>
2019-09-27 12:52:25 +0100

    Removed HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,21 @@ import { theme } from "./themes";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
-import HomePage from "./pages/HomePage";
+import WordAnalysisPage from "./pages/wordAnalysis/WordAnalysisPage";
+import PageLayout, { ITabItem } from "./components/layout/PageLayout";
 
 const App: React.FC = () => {
+    const mainNavTabs: ITabItem[] = [
+        { route: "/wordanalysis", label: "Word analysis", content: <WordAnalysisPage /> },
+        { route: "/manual", label: "Manual", content: "Manual!" },
+        { route: "/contactus", label: "Contact us", content: "Contact us!" }
+    ];
+
     return (
         <MuiThemeProvider theme={theme}>
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" component={HomePage} />
+                    <Route path="/" render={() => <PageLayout tabItems={mainNavTabs} />} />
                 </Switch>
             </BrowserRouter>
         </MuiThemeProvider>
