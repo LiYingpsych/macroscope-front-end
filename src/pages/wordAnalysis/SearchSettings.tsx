0fb0313a4c8eaebe93b02429612224043364b299
Jack Crompton <jackcrompton8@gmail.com>
2019-10-08 11:03:20 +0100

    Changed numberOfSynonyms to be valid

--- src/pages/wordAnalysis/SearchSettings.tsx
+++ src/pages/wordAnalysis/SearchSettings.tsx
@@ -66,7 +66,7 @@ export default function SearchSettings() {
             isOpen: false,
             settings: {
                 year: closestMaxYear,
-                numberOfSynonyms: 105
+                numberOfSynonyms: 5
             }
         }
     };
