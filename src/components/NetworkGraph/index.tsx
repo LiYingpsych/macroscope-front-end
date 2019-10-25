6c7d481a11eca4bbe499bc773423ca6b0152c3ae
Jack Crompton <jackcrompton8@gmail.com>
2019-10-25 20:06:00 +0100

    Moved network graph classes into NetworkGraph

--- src/components/NetworkGraph/index.tsx
+++ src/components/NetworkGraph/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import JSNetworkGraph from "./JSNetworkGraph";
+import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            "& svg": {
+                width: "100% !important"
+            }
+        }
+    })
+);
 
 interface IProps<T> {
     id: string;
@@ -22,6 +33,7 @@ export interface IGraphData<T> {
 
 interface INetworkGraphNode<T> {
     id: T;
+    size?: number;
 }
 
 interface INetworkGraphLinks<T> {
@@ -29,10 +41,15 @@ interface INetworkGraphLinks<T> {
     target: T;
 }
 
-const NetworkGraph = <T,>(props: IProps<T>) => {
+const NetworkGraph = <T, _>(props: IProps<T>) => {
     const { id, data, config = {} } = props;
+    const classes = useStyles();
 
-    return <JSNetworkGraph id={id} data={data} config={config} />;
+    return (
+        <div className={classes.root}>
+            <JSNetworkGraph id={id} data={data} config={config} />
+        </div>
+    );
 };
 
 export default NetworkGraph;
--- src/pages/wordAnalysis/dataDisplays/SynonymNetworkGraph.tsx
+++ src/pages/wordAnalysis/dataDisplays/SynonymNetworkGraph.tsx
@@ -5,25 +5,12 @@ import ISynonymNetworkData, {
     ISynonymNetworkEdge
 } from "../../../models/ISynonymNetworkData";
 import NetworkGraph, { IGraphData } from "../../../components/NetworkGraph";
-import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            "& svg": {
-                width: "100% !important"
-            }
-        }
-    })
-);
 
 interface IProps {
     data: ISynonymNetworkData;
 }
 
 export default function SynonymNetworkGraph(props: IProps) {
-    const classes = useStyles();
-
     const { data } = props;
     const networkGraphData: IGraphData<string> = {
         nodes: data.synonymNetwork.nodes.map((node: ISynonymNetworkNode) => {
@@ -40,12 +27,10 @@ export default function SynonymNetworkGraph(props: IProps) {
     };
 
     return (
-        <div className={classes.root}>
-            <NetworkGraph
-                id="Synonym-network-graph"
-                data={networkGraphData}
-                config={{ automaticRearrangeAfterDropNode: true }}
-            />
-        </div>
+        <NetworkGraph
+            id="Synonym-network-graph"
+            data={networkGraphData}
+            config={{ automaticRearrangeAfterDropNode: true }}
+        />
     );
 }
