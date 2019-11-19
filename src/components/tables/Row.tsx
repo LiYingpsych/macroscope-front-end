e9209bf008f1e7cfdd610669808561f65e397516
Jack Crompton <jackcrompton8@gmail.com>
2019-11-19 21:09:20 +0000

    Clickable row uses cursor pointer style

--- src/components/tables/Row.tsx
+++ src/components/tables/Row.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+
 import IColumn from "./models/IColumn";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        clickableRow: {
+            cursor: "pointer"
+        }
+    })
+);
 
 interface IProps<T> {
     columnData: IColumn<T>[];
@@ -11,13 +21,19 @@ interface IProps<T> {
 
 export default function Row<T>(props: IProps<T>) {
     const { rowItemData, columnData, onRowClick } = props;
+    const classes = useStyles();
 
     const handleClick = (event: React.MouseEvent<HTMLTableRowElement, MouseEvent>) => {
         if (typeof onRowClick !== "undefined") onRowClick(rowItemData);
     };
 
     return (
-        <TableRow tabIndex={-1} hover={typeof onRowClick !== "undefined"} onClick={handleClick}>
+        <TableRow
+            tabIndex={-1}
+            hover={typeof onRowClick !== "undefined"}
+            onClick={handleClick}
+            className={typeof onRowClick !== "undefined" ? classes.clickableRow : ""}
+        >
             {columnData.map((column, i) => {
                 return <TableCell key={i}>{rowItemData[column.dataPropertyKey]}</TableCell>;
             })}
