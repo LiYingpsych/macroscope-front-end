9d5dc79adaac3ec67dddb96148c93f8ae51f1fdc
Jack Crompton <jackcrompton8@gmail.com>
2019-11-20 17:09:33 +0000

    Updated 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,6 +1,33 @@
 import React from "react";
-import PageContent from "../components/layout/PageContent";
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
 
+import PageContent from "../components/layout/PageContent";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            paddingTop: theme.spacing(6)
+        }
+    })
+);
 export default function NotFoundPage() {
-    return <PageContent>404: Page not found</PageContent>;
+    const classes = useStyles();
+
+    return (
+        <PageContent>
+            <Grid container direction="column" alignItems="center" className={classes.root}>
+                <Grid item>
+                    <Typography variant="h1">404</Typography>
+                </Grid>
+
+                <Grid item>
+                    <Typography variant="subtitle1" align="center">
+                        We could not find the page you were looking for.
+                    </Typography>
+                </Grid>
+            </Grid>
+        </PageContent>
+    );
 }
