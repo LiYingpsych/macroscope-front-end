6093c58e14d2471fb30e5778411978fec0a4fffa
Jack Crompton <jackcrompton8@gmail.com>
2019-11-21 16:11:45 +0000

    Added width calculation to NetworkGraph - not cleaned up

--- src/components/NetworkGraph/index.tsx
+++ src/components/NetworkGraph/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import JSNetworkGraph from "./JSNetworkGraph";
 import { Theme, makeStyles, createStyles } from "@material-ui/core/styles";
+import assignDefaultValuesToObject from "../../utils/assignDefaultValuesToObject";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -41,15 +42,56 @@ interface INetworkGraphLinks<T> {
     target: T;
 }
 
-const NetworkGraph = <T, _>(props: IProps<T>) => {
+export default function NetworkGraph<T>(props: IProps<T>) {
     const { id, data, config = {} } = props;
     const classes = useStyles();
 
+    const rootElement = useRef(null);
+
+    const [width, setWidth] = useState<number | undefined>(undefined);
+
+    const defaultConfig: IGraphConfig = {
+        automaticRearrangeAfterDropNode: false,
+        collapsible: false,
+        directed: false,
+        height: 250
+    };
+
+    // React.useEffect(() => {
+    //     window.addEventListener("resize", updateWidthAndHeight);
+    //     return () => window.removeEventListener("resize", updateWidthAndHeight);
+    // });
+
+    // React.useEffect(() => {
+    //     window.addEventListener("load", updateWidthAndHeight);
+    //     return () => window.removeEventListener("load", updateWidthAndHeight);
+    // });
+
+    React.useEffect(() => {
+        console.log("hello");
+        updateWidthAndHeight();
+    }, [rootElement]);
+
+    const updateWidthAndHeight = () => {
+        console.log("updateWidthAndHeight");
+        //@ts-ignore
+        const currentWidth = rootElement.current.offsetWidth;
+
+        console.log(currentWidth);
+
+        setWidth(currentWidth);
+        // setHeight(window.innerHeight);
+    };
+
     return (
-        <div className={classes.root}>
-            <JSNetworkGraph id={id} data={data} config={config} />
+        <div className={classes.root} ref={rootElement}>
+            {typeof width === "undefined" ? null : (
+                <JSNetworkGraph
+                    id={id}
+                    data={data}
+                    config={assignDefaultValuesToObject(defaultConfig, { ...config, width: width })}
+                />
+            )}
         </div>
     );
-};
-
-export default NetworkGraph;
+}
