3d10d4b9127a40a4c4f294911dad67a885a78cb9
Jack Crompton <jackcrompton8@gmail.com>
2019-11-26 15:51:25 +0000

    Part way through adding point line for line chart - very incomplete, stay away

--- src/components/victoryCharts/LineChart.tsx
+++ src/components/victoryCharts/LineChart.tsx
@@ -7,7 +7,11 @@ import {
     VictoryZoomContainer,
     VictoryBrushContainer,
     VictoryAxisProps,
-    VictoryLabel
+    VictoryLabel,
+    VictoryScatter,
+    VictoryCursorContainer,
+    CursorData,
+    VictoryCursorContainerProps
 } from "victory";
 import ICartesianCoordinate from "./models/ICartesianCoordinate";
 import { chartColours } from "../../themes/colours";
@@ -40,12 +44,6 @@ export default function LineChart<S, T>(props: IProps<S, T>) {
             ? (x: any) => new Date(x, 0).getFullYear()
             : independentAxisProps.tickFormat;
 
-    const [zoomDomain, setZoomDomain] = useState<DomainPropType>();
-
-    const handleZoom = (domain: DomainPropType) => {
-        setZoomDomain(domain);
-    };
-
     let legendData: ILegendDataProp[] = [];
 
     const LinesComponent = lines.map((line, i) => {
@@ -59,15 +57,48 @@ export default function LineChart<S, T>(props: IProps<S, T>) {
         );
     });
 
+    // type cursorDimensionType = "x" | "y" | undefined;
+    // const cursorDimension: cursorDimensionType = "x";
+    const [cursorCoordinate, setCursorCoordinate] = useState<CursorData>();
+
+    // TODO: submit issue - value returned depends on cursorDimension value when it should still return a CursorData prop
+    const handleCursorChange = (value: CursorData, props: VictoryCursorContainerProps) => {
+        console.log(value);
+        setCursorCoordinate(value);
+        // if (typeof cursorDimension === "undefined") setCursorCoordinate({ x: value.x, y: value.y });
+        // else if (cursorDimension === "x")
+        //     //@ts-ignore
+        //     setCursorCoordinate({ x: value, y: undefined });
+        // else if (cursorDimension === "y")
+        //     //@ts-ignore
+        //     setCursorCoordinate({ x: undefined, y: value });
+    };
+
+    // const [zoomDomain, setZoomDomain] = useState<DomainPropType>();
+    // const handleZoom = (domain: DomainPropType) => {
+    //     setZoomDomain(domain);
+    // };
     return (
         <>
             <ChartWrapper
                 padding={padding}
+                // containerComponent={
+                //     <VictoryZoomContainer
+                //         zoomDimension="x"
+                //         zoomDomain={zoomDomain}
+                //         onZoomDomainChange={handleZoom}
+                //     />
+                // }
                 containerComponent={
-                    <VictoryZoomContainer
-                        zoomDimension="x"
-                        zoomDomain={zoomDomain}
-                        onZoomDomainChange={handleZoom}
+                    <VictoryCursorContainer
+                        // cursorDimension={cursorDimension}
+                        cursorLabel={data => {
+                            return typeof cursorCoordinate !== "undefined"
+                                ? `${cursorCoordinate.x}, ${cursorCoordinate.y}`
+                                : "";
+                        }}
+                        onCursorChange={handleCursorChange}
+                        cursorComponent={<div style={{ display: "none" }}></div>}
                     />
                 }
                 legendData={legendData}
@@ -84,10 +115,29 @@ export default function LineChart<S, T>(props: IProps<S, T>) {
                 />
 
                 {LinesComponent}
+
+                {/* X coordinate line of mouseover */}
+                {/* <VictoryLine
+                    style={{
+                        data: { stroke: "red", strokeWidth: 2 },
+                        labels: { fill: "red", fontSize: 20 }
+                    }}
+                    labels={["Important"]}
+                    labelComponent={<VictoryLabel angle={-90} y={100} />}
+                    x={() => 2000}
+                /> */}
+                {/* Intersection point */}
+                {/* <VictoryScatter
+                    symbol="star"
+                    size={8}
+                    style={{ data: { fill: "red" } }}
+                    data={[{ x: 2000, y: 6 }]}
+                /> */}
             </ChartWrapper>
-            <ChartWrapper
+            {/* <ChartWrapper
                 padding={{ top: 0, left: padding.left, right: padding.right, bottom: 30 }}
                 height={100}
+                width={width}
                 containerComponent={
                     <VictoryBrushContainer
                         brushDimension="x"
@@ -98,7 +148,7 @@ export default function LineChart<S, T>(props: IProps<S, T>) {
             >
                 <VictoryAxis tickFormat={(t: any) => ""} />
                 {LinesComponent}
-            </ChartWrapper>
+            </ChartWrapper> */}
         </>
     );
 }
