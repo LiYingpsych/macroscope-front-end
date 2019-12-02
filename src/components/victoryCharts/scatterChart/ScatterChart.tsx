4351f36334d56236c718355cb3ca0f6885a0f531
Jack Crompton <jackcrompton8@gmail.com>
2019-12-02 22:11:20 +0000

    Added hover events to scatter graph

--- src/components/victoryCharts/scatterChart/ScatterChart.tsx
+++ src/components/victoryCharts/scatterChart/ScatterChart.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import ChartWrapper from "../ChartWrapper";
-import { VictoryScatter, VictoryScatterProps } from "victory";
+import {
+    VictoryScatter,
+    VictoryScatterProps,
+    StringOrNumberOrCallback,
+    EventCallbackInterface,
+    VictoryAxis
+} from "victory";
 import { useTheme } from "@material-ui/core/styles";
 import useZoomable from "../commonHooks/useZoomable";
 
@@ -15,18 +21,58 @@ interface IProps extends VictoryScatterProps {
     data: IScatterDataPoint[];
 }
 
+type VictoryScatterEventTarget = "data" | "labels" | "parent";
+
 export default function ScatterChart(props: IProps) {
     const { data, size = 7, ...rest } = props;
 
     const theme = useTheme();
 
-    const padding = { left: 60, top: 20, right: 10, bottom: 60 };
+    const padding = { left: 0, top: 0, right: 0, bottom: 0 };
+
+    const highlightLabel = (): EventCallbackInterface<"labels", StringOrNumberOrCallback> => {
+        return {
+            target: "labels",
+            mutation: (props: any) => {
+                return {
+                    style: Object.assign({}, props.style, {
+                        // fill: "tomato",
+                        fontWeight: "bold",
+                        backgroundColor: "red"
+                    })
+                };
+            }
+        };
+    };
+
+    const highlightDataPoint = (): EventCallbackInterface<"data", StringOrNumberOrCallback> => {
+        return {
+            target: "data",
+            mutation: (props: any) => {
+                return {
+                    style: Object.assign({}, props.style, {
+                        opacity: 1
+                    })
+                };
+            }
+        };
+    };
+
+    const resetTarget = (target: VictoryScatterEventTarget) => {
+        return {
+            target: target,
+            mutation: () => {
+                return null;
+            }
+        };
+    };
 
     const Content = (
         <VictoryScatter
             data={data}
             style={{
                 data: {
+                    opacity: 0.7,
                     fill: ({ datum }) =>
                         typeof datum.fill === "undefined"
                             ? theme.palette.secondary.light
@@ -34,21 +80,31 @@ export default function ScatterChart(props: IProps) {
                 }
             }}
             size={size}
+            events={[
+                {
+                    target: "data",
+                    eventHandlers: {
+                        onMouseOver: () => {
+                            return [highlightLabel(), highlightDataPoint()];
+                        },
+                        onMouseLeave: () => {
+                            return [resetTarget("labels"), resetTarget("data")];
+                        }
+                    }
+                }
+            ]}
             {...rest}
         />
     );
 
-    const { zoomableContainerComponent } = useZoomable({
-        brushOptions: {
-            padding: { top: 0, left: padding.left, right: padding.right, bottom: 0 },
-            children: Content
-        }
-    });
+    const { zoomableContainerComponent } = useZoomable();
 
     return (
         <>
             <ChartWrapper padding={padding} containerComponent={zoomableContainerComponent}>
                 {Content}
+                <VictoryAxis tickFormat={(t: any) => ""} />
+                <VictoryAxis dependentAxis tickFormat={(t: any) => ""} />
             </ChartWrapper>
         </>
     );
