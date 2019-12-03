cfe6d3bc6f0f8ac29a2ec624cef87701e597b4fd
Jack Crompton <jackcrompton8@gmail.com>
2019-12-03 17:47:20 +0000

    ChangeBar now takes a type option which is used to calculate how to display the bar

--- src/components/victoryCharts/changeBarChart/ChangeBarChart.tsx
+++ src/components/victoryCharts/changeBarChart/ChangeBarChart.tsx
@@ -15,10 +15,6 @@ interface IProps {
 
 export default function ChangeBarChart(props: IProps) {
     const { increasingData, decreasingData } = props;
-    const theme = useTheme();
-
-    const positiveColour = theme.palette.primary.light;
-    const negativeColour = theme.palette.secondary.light;
 
     return (
         <ChartWrapper horizontal>
@@ -29,33 +25,52 @@ export default function ChangeBarChart(props: IProps) {
                     tickLabels: { stroke: "none", fill: "transparent" }
                 }}
             />
-            {ChangeBar({ colour: negativeColour, data: decreasingData, orientation: "right" })}
-            {ChangeBar({ colour: positiveColour, data: increasingData })}
+            {ChangeBar({ data: decreasingData, type: "negative" })}
+            {ChangeBar({ data: increasingData, type: "positive" })}
         </ChartWrapper>
     );
 }
 
 interface IBarProps {
-    colour: string;
     data: IChangeBarData[];
-    orientation?: "top" | "bottom" | "left" | "right";
+    type: "positive" | "negative";
 }
 
 function ChangeBar(props: IBarProps) {
-    const { colour, data, orientation = "left" } = props;
+    const { data, type = "" } = props;
+
+    const theme = useTheme();
+
+    const orientation = type === "positive" ? "right" : "left";
+    const colour =
+        type === "positive" ? theme.palette.primary.light : theme.palette.secondary.light;
+
+    const tickValues = data.map(d => d.label);
+
+    const maxLength = Math.max(...data.map(d => Math.abs(d.length)));
+
+    const parsedData = data.map(d => {
+        return {
+            x: d.label,
+            y:
+                maxLength === 0
+                    ? 0
+                    : ((type === "positive" ? 1 : -1) * Math.abs(d.length)) / maxLength
+        };
+    });
 
     return [
         <VictoryAxis
-            tickValues={data.map(d => d.label)}
+            key="axis"
+            tickValues={tickValues}
             style={{
                 axis: { stroke: "none" }
             }}
             orientation={orientation}
         />,
         <VictoryBar
-            data={data}
-            x="label"
-            y="length"
+            key="bar"
+            data={parsedData}
             style={{
                 data: {
                     fill: colour
