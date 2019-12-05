516bdac48b49e74862ce3f7d201256e473454523
Jack Crompton <jackcrompton8@gmail.com>
2019-12-05 20:58:06 +0000

    Refactored manual page

--- src/pages/manual/ManualPage.tsx
+++ src/pages/manual/ManualPage.tsx
@@ -1,44 +1,53 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
-import { makeStyles, createStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
 import PageContent from "../../components/layout/PageContent";
 import MacroscopeLogo from "../../components/images/MacroscopeLogo";
-import List from "../../components/typography/List";
+import List from "./components/List";
 import GetApp from "@material-ui/icons/GetApp";
 import IconButton from "@material-ui/core/IconButton";
+import { useTheme } from "@material-ui/core";
 
-const useStyles = makeStyles(() =>
-    createStyles({
-        gridItem: {
-            width: "100%"
-        }
-    })
-);
+interface ISectionProps {
+    title?: string;
+    children?: ReactNode;
+}
+
+function Section(props: ISectionProps) {
+    const { title, children } = props;
+    const theme = useTheme();
+    return (
+        <Grid
+            container
+            item
+            direction="column"
+            spacing={2}
+            style={{ marginBottom: theme.spacing(4) }}
+        >
+            {typeof title === "undefined" ? null : <Typography variant="h4">{title}</Typography>}
+            {React.Children.map(children, child => (
+                <Grid item xs={12} style={{ paddingLeft: 0, paddingRight: 0 }}>
+                    {child}
+                </Grid>
+            ))}
+        </Grid>
+    );
+}
 
 export default function ManualPage() {
-    const classes = useStyles();
-
     return (
         <PageContent>
             <Grid container direction="column" spacing={2}>
-                <Grid container justify="center" item xs={12} className={classes.gridItem}>
+                <Grid container justify="center" item xs={12}>
                     <Grid item>
                         <MacroscopeLogo size={300} />
                     </Grid>
                 </Grid>
-                <Grid item xs={12} className={classes.gridItem}>
-                    <Intro />
-                </Grid>
-                <Grid item xs={12} className={classes.gridItem}>
-                    <QuickStartGuide />
-                </Grid>
-
-                <Grid item xs={12} className={classes.gridItem}>
-                    <ConceptualFramework />
-                </Grid>
+                <Intro />
+                <QuickStartGuide />
+                <ConceptualFramework />
             </Grid>
         </PageContent>
     );
@@ -48,44 +57,34 @@ function Intro() {
     const numberOfPackages = 7;
 
     return (
-        <Grid container direction="column" spacing={2}>
-            <Grid item xs={12}>
-                <Typography>
-                    The Macroscope is a powerful tool built to interpret historical structure of
-                    language. It is designed to provide comprehensive analyses of historical meaning
-                    with the click of a button. This enables quick insights for a curious explorer,
-                    requiring little to no knowledge of linguistics. At the same time, the function
-                    to download the raw data of any figure caters to researchers, content creators
-                    or specialists who don't want to design their own solutions from scratch.
-                </Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Typography>
-                    We offer a suite of {numberOfPackages} analytical packages, each explaining a
-                    different piece of the puzzle. The analyses comes with predefined settings to
-                    work out of the box. Where possible, the cogs are exposed to the user allowing
-                    for the finetuning of parameters.
-                </Typography>
-            </Grid>
-        </Grid>
+        <Section title="Intro">
+            <Typography>
+                The Macroscope is a powerful tool built to interpret historical structure of
+                language. It is designed to provide comprehensive analyses of historical meaning
+                with the click of a button. This enables quick insights for a curious explorer,
+                requiring little to no knowledge of linguistics. At the same time, the function to
+                download the raw data of any figure caters to researchers, content creators or
+                specialists who don't want to design their own solutions from scratch.
+            </Typography>
+            <Typography>
+                We offer a suite of {numberOfPackages} analytical packages, each explaining a
+                different piece of the puzzle. The analyses comes with predefined settings to work
+                out of the box. Where possible, the cogs are exposed to the user allowing for the
+                finetuning of parameters.
+            </Typography>
+        </Section>
     );
 }
 
 function QuickStartGuide() {
     return (
-        <Grid container direction="column" spacing={2}>
-            <Grid item xs={12}>
-                <Typography variant="h4">Quick-Start Guide</Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Typography>
-                    You can use the tool immediately by entering a word in the search bar on the
-                    "Word Analysis" page. This will perform "Sentiment" and "Frequency" analyses
-                    with the default settings.
-                </Typography>
-            </Grid>
-
-            <Grid item xs={12}>
+        <Section title="Quick-Start Guide">
+            <Typography>
+                You can use the tool immediately by entering a word in the search bar on the "Word
+                Analysis" page. This will perform "Sentiment" and "Frequency" analyses with the
+                default settings.
+            </Typography>
+            <>
                 <Typography variant="subtitle2">
                     You can use the steps below to perform more detailed analyses.
                 </Typography>
@@ -115,56 +114,46 @@ function QuickStartGuide() {
                     ]}
                     ordered
                 />
-            </Grid>
-            <Grid item xs={12}>
-                <Typography>
-                    It is recommended to try analysing words with the default settings then tweaking
-                    the settings where necessary. Many of the settings will have optimal values on a
-                    case by case basis (e.g. the similarity thresholds of networks). A setting that
-                    works for the word "risk" may not be ideal for a less frequent word like
-                    "bitter".
-                </Typography>
-            </Grid>
-        </Grid>
+            </>
+            <Typography>
+                It is recommended to try analysing words with the default settings then tweaking the
+                settings where necessary. Many of the settings will have optimal values on a case by
+                case basis (e.g. the similarity thresholds of networks). A setting that works for
+                the word "risk" may not be ideal for a less frequent word like "bitter".
+            </Typography>
+        </Section>
     );
 }
 
 function ConceptualFramework() {
     return (
-        <Grid container direction="column" spacing={2}>
-            <Grid item xs={12}>
-                <Typography variant="h4">Conceptual Framework</Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Typography>
-                    The Macroscope provides researchers with the ability to examine two distinct but
-                    related aspects of linguistic change in individual words over historical time.
-                    <List
-                        items={[
-                            <Typography>
-                                Diachronic word embeddings, computed from the co-occurrence matrix,
-                                enables us to discover words that are semantically similar to a
-                                given word for a given year (i.e. the semantic or synonym structure
-                                surrounding a word). These semantically related words are referred
-                                to as synonyms.
-                            </Typography>,
-                            <Typography>
-                                The co-occurrence matrix also provides information regarding the
-                                context of a given word at a given year. Words that co-occur with
-                                the target word are referred to as context words.
-                            </Typography>
-                        ]}
-                        ordered
-                    />
-                </Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Typography>
-                    Hence, the Macroscope can be used to examine the semantic (synonym) and
-                    contextual (co-occurrence) structure of individual words for a specific year and
-                    over historical time.
-                </Typography>
-            </Grid>
-        </Grid>
+        <Section title="Conceptual Framework">
+            <Typography>
+                The Macroscope provides researchers with the ability to examine two distinct but
+                related aspects of linguistic change in individual words over historical time.
+                <List
+                    items={[
+                        <Typography>
+                            Diachronic word embeddings, computed from the co-occurrence matrix,
+                            enables us to discover words that are semantically similar to a given
+                            word for a given year (i.e. the semantic or synonym structure
+                            surrounding a word). These semantically related words are referred to as
+                            synonyms.
+                        </Typography>,
+                        <Typography>
+                            The co-occurrence matrix also provides information regarding the context
+                            of a given word at a given year. Words that co-occur with the target
+                            word are referred to as context words.
+                        </Typography>
+                    ]}
+                    ordered
+                />
+            </Typography>
+            <Typography>
+                Hence, the Macroscope can be used to examine the semantic (synonym) and contextual
+                (co-occurrence) structure of individual words for a specific year and over
+                historical time.
+            </Typography>
+        </Section>
     );
 }
